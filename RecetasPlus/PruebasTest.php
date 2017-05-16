9cd421c338806918d13ed3725007c5b88dd4013f
Miguel Beltran <miguel.beltran01@uptc.edu.co>
2017-05-16 09:19:50 -0500

    test

--- RecetasPlus/PruebasTest.php
+++ RecetasPlus/PruebasTest.php
@@ -9,19 +9,6 @@ class PruebasTest extends PHPUnit_Framework_TestCase{
         $actual = $d->getHost();
         $this->assertEquals($expect, $actual);
     }    
-  
-   public function test_TypeRecipes(){
-    	$test = new functions();
-    	$expect = false;
-    	$result = empty($test->getTypesRecipes("SELECT * FROM RECETAS WHERE TIPO_RECETA = 'Postre'"));
-    	$this->assertEquals($expect, $result);
-    }
-
-	public function test_returnRecipe(){
-    	$test = new functions();
-    	$expect = false;
-    	$result = empty($test->returnRecipe());
-    	$this->assertEquals($expect, $result);
-    }    
+     
 }
 ?>
