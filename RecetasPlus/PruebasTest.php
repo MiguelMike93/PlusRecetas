04b450a86073a66ccb8b214c94a88d5107829815
Miguel Beltran <miguel.beltran01@uptc.edu.co>
2017-05-16 06:13:31 -0500

    prueba que sirve

--- RecetasPlus/PruebasTest.php
+++ RecetasPlus/PruebasTest.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'DataDb.php';
-require_once 'functions.php';
+//require_once 'functions.php';
 class PruebasTest extends PHPUnit_Framework_TestCase{
   
     public function test_Host() {
@@ -8,23 +8,7 @@ class PruebasTest extends PHPUnit_Framework_TestCase{
     	$expect = 'localhost';
         $actual = $d->getHost();
         $this->assertEquals($expect, $actual);
-    }
-
-
-    public function test_TypeRecipes(){
-    	$test = new functions();
-    	$expect = false;
-    	$result = !empty($test->getTypesRecipes("SELECT * FROM RECETAS WHERE TIPO_RECETA = 'Postre'"));
-    	$this->assertEquals($expect, $result);
-    }
-
-	public function test_returnRecipe(){
-    	$test = new functions();
-    	$expect = false;
-    	$result = !empty($test->returnRecipe());
-    	$this->assertEquals($expect, $result);
     }    
-    
   
 }
 ?>
