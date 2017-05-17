6f80475d783ef24a7278e3542dd6f54231134576
Daniel Pabon <danielpabon745@gmail.com>
2017-05-17 18:12:23 -0500

    Update PruebasTest.php

--- RecetasPlus/PruebasTest.php
+++ RecetasPlus/PruebasTest.php
@@ -3,13 +3,22 @@ require_once 'DataDb.php';
 class PruebasTest extends PHPUnit_Framework_TestCase{
   
     public function test_Host() {
-    
         $d = new DataDb();
-        
-    $expect = 'localhost';
+        $expect = '185.27.134.11';
         $actual = $d->getHost();
         $this->assertEquals($expect, $actual);
     }
-  
+    public function test_User() {
+        $d = new DataDb();
+        $expect = 'epiz_19830617';
+        $actual = $d->getUsername();
+        $this->assertEquals($expect, $actual);
+    }
+    public function test_DB() {
+        $d = new DataDb();
+        $expect = 'epiz_19830617_recetasPlusdb';
+        $actual = $d->getDB();
+        $this->assertEquals($expect, $actual);
+    } 
 }
 ?>
