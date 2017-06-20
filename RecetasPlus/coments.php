b4856809e2f24ace270e59375fe3a336dc130be7
megan ibague <menibapi@gmail.edu.co>
2017-06-20 09:50:35 -0500

    mostrar correo2

--- RecetasPlus/coments.php
+++ RecetasPlus/coments.php
@@ -42,6 +42,39 @@
 		}
 		header("Location: recipe1.php?variable=".$receta);
 	} 
-	else
+	else{
+		require("/PHPMailer_5.2.0/class.phpmailer.php");
+		$mail = new phpMailer();
+//Luego tenemos que iniciar la validación por SMTP:
+$mail->IsSMTP();
+$mail->SMTPAuth = true;
+$mail->Host = "smtp.gmail.com"; // SMTP a utilizar. Por ej. smtp.elserver.com
+$mail->Username = "menibapi@gmail.com"; // Correo completo a utilizar
+$mail->Password = "Meguitan1995"; // Contraseña
+$mail->Port = 465; // Puerto a utilizar
+
+//Con estas pocas líneas iniciamos una conexión con el SMTP. Lo que ahora deberíamos hacer, es configurar el mensaje a enviar, el //From, etc.
+$mail->From = "menibapi@gmail.com"; // Desde donde enviamos (Para mostrar)
+$mail->FromName = "Nombre";
+
+//Estas dos líneas, cumplirían la función de encabezado (En mail() usado de esta forma: “From: Nombre <correo@dominio.com>”) de //correo.
+$mail->AddAddress("megan.ibague@uptc.edu.co"); // Esta es la dirección a donde enviamos
+$mail->IsHTML(true); // El correo se envía como HTML
+$mail->Subject = "Titulo"; // Este es el titulo del email.
+$body = "Hola mundo. Esta es la primer línea<br />";
+$body .= "Acá continuo el <strong>mensaje</strong>";
+$mail->AltBody = "Hola mundo. Esta es la primer línean Acá continuo el mensaje"; // Texto sin html
+$mail->Body = $body; // Mensaje a enviar
+$exito = $mail->Send(); // Envía el correo.
+
+//También podríamos agregar simples verificaciones para saber si se envió:
+if($exito){
+echo 'El correo fue enviado correctamente.';
+
+}else{
+echo 'Hubo un inconveniente. Contacta a un administrador.';
+
+}
 		header("Location: index.php");
+	}
 ?>
